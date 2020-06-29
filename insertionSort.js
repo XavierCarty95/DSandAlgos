5bf9e01bfca3a15216c723cc830aa459d9dd1e2c
Xavier Carty <xaviercarty@gmail.com>
2020-06-29 14:29:12 -0400

    changed code

--- insertionSort.js
+++ insertionSort.js
@@ -1,20 +1,19 @@
 function insertionSort(array) {
     // Write your code here.
-      let sorted = []
-      for(let i = 0; i < array.length; i++){
-              sorted.push(array[i])
-           for(let j = i + 1; j < array.length; j++){
-                if(array[j] > sorted[i]){
-                       sorted[i+1] = array[j]
-                  }
-                else {
-                       let temp = sorted[i]
-                       sorted[i+1] = temp
-                       sorted[i] = array[j]
-                  }
-           }
-          
-      }
-       return sorted
       
+      for(let i = 1; i < array.length; i++){
+          let j = i;
+          while(j > 0 && array[j] < array[j-1]){
+                      swap(j , j-1 , array)
+                      j -= 1
+          }
+      }
+      return array;
   }
+  function swap(i , j , array) {
+       const temp = array[j]
+         array[j] = array[i]
+         array[i] = temp
+    
+    }
+    

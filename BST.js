2239be52a70cf002acd7d3edeee620f96e83c25e
Xavier Carty <xaviercarty@gmail.com>
2020-07-28 16:46:05 -0400

    added files

--- BST.js
+++ BST.js
@@ -1,102 +1,66 @@
-function BST(value)  {
-    this.value = value; 
+class Node {
+  constructor(value){
+    this.value = value;
     this.left = null; 
-    this.right = null; 
+    this.right = null;
   }
-  
-  BST.prototype.insert = function(value) 
-  {  
-    if(value <= this.value){
-      if(!this.left) this.left = new BST(value);
-      else this.left.insert(value)
-  
-     
-  } else if (value > this.value){
-    if(!this.right)this.right = new BST(value);
-    else this.right.insert(value);
-  
-    }
-  };
-  
-  BST.prototype.contains = function(value) 
-  {
-    if(value == this.value) return true
-    else if (value < this.value)
-    {
-      if(!this.left)return false
-      else return this.left.contains(value)
-    }
-    else if (value > this.value)
-    {
-       if(!this.right) return false
-       else return this.right.contains(value)
-    }
-  };
-  
-  BST.prototype.depthFirstTraversal = function(iteratorFunc, order) {
-   
-   
-    if(order === 'pre-order') iteratorFunc(this.value); 
-    if(this.left) this.left.depthFirstTraversal(                   iteratorFunc,order)
-      if(order === 'in-order')
-    iteratorFunc(this.value);
-    if(this.right)this.right.depthFirstTraversal(iteratorFunc,order);
-    if(order === 'post-order')iteratorFunc(this.value);
-  };
-  
-  BST.prototype.breadthFirstTraversal = function(iteratorFunc)
-  {
-    var queue = [this];
-    while(queue.length){
-      var treeNode =  queue.shift();
-      iteratorFunc(treeNode)
-      if(treeNode.left) queue.push(treeNode.left)
-      if(treeNode.right) queue.push(treeNode.right)
-  
-  
-    }
-  
+}
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
   }
+
+  insert(value){
+    let newNode = new Node(value)
+    if(this.root === null){
+      this.root = newNode
+      return this;
+    } 
+      let current = this.root
+      while(true){
+        if(value === current.value) return undefined
+        if(value < current.value){
+          if(current.left === null){
+              current.left = newNode;
+              return this
+          } 
+            current = current.left
+          } else {
+                if(current.right === null){
+                  current.right = newNode
+                  return this;
+                } 
+                  current = current.right
+              
+          }
+        }
+    }
+
+    find(value){
   
-  BST.prototype.getMinVal = function() {
-   if(this.left) return this.left.getMinVal();
-   else return this.value;
-  
-  
-  };
-  
-  BST.prototype.getMaxVal = function() {
-    if(this.right) return this.right.getMaxVal();
-   else return this.value
-  
-  
+      if(this.root === null) return false; 
+      let current = this.root
+      let found = false; 
+      while(current && !found){
+        if(value < current.value){
+          current = current.left
+         } else if(value > current.value){
+           current = current.right
+         } else {
+           found = true
+         }
+      }
+      if(!found) return undefined;
+      return current
+    }
   }
-  
-  
-  
-  
-  
-  var bst = new BST(50); 
-  
-  bst.insert(30)
-  bst.insert(70)
-  bst.insert(100)
-  bst.insert(60)
-  bst.insert(59)
-  bst.insert(20)
-  bst.insert(45)
-  bst.insert(35)
-  bst.insert(85)
-  bst.insert(105)
-  bst.insert(10);
-  
-  
-  
-  
-  function log(node) {
-    console.log(node.value)
-  
-  }
-  
-  console.log('MIN: ', bst.getMinVal())
-  console.log('MAX: ', bst.getMaxVal())
+
+
+
+
+
+
+// var tree = new BinarySearchTree() 
+// tree.root = new Node(10)
+// tree.root.right = new Node(15)
+// tree.root.left = new Node(7)

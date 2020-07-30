75db9eab75931ccef4ca229443b066accec62a4d
Xavier Carty <xaviercarty@gmail.com>
2020-07-30 08:59:50 -0400

    forgot to commit

--- BST.js
+++ BST.js
@@ -53,6 +53,59 @@ class BinarySearchTree {
       if(!found) return undefined;
       return current
     }
+    BFS(){
+      let data = [] 
+      let queue = []
+      let node = this.root 
+      queue.push(node)
+      while(queue.length){
+        node = queue.shift()
+        data.push(node.value)
+        if(node.left) queue.push(node.left)
+        if(node.right) queue.push(node.right)
+      }
+      return data;
+    }
+    DFSPreOrder(){
+        let nodeVisted = []
+       function helper(node){
+          nodeVisted.push(node.value)
+          if(node.left) helper(node.left)
+          if(node.right) helper(node.right)
+        }
+        helper(this.root)
+        return nodeVisted
+
+    }
+    DFSPostOrder(){
+      let nodeVisted = []
+      
+     function helper(node){
+      
+        if(node.left) helper(node.left)
+        if(node.right) helper(node.right)
+        nodeVisted.push(node.value)
+      }
+  
+      helper(this.root)
+      return nodeVisted
+
+  }
+  DFSInOrder(){
+    let nodeVisted = []
+    
+   function helper(node){
+    
+      if(node.left) helper(node.left)
+      nodeVisted.push(node.value)
+      if(node.right) helper(node.right)
+  }
+
+    helper(this.root)
+    return nodeVisted
+
+}
+
   }
 
 
@@ -60,7 +113,3 @@ class BinarySearchTree {
 
 
 
-// var tree = new BinarySearchTree() 
-// tree.root = new Node(10)
-// tree.root.right = new Node(15)
-// tree.root.left = new Node(7)

3a959fb9ef79ee837ebe4693825edbb38d6507ca
Xavier Carty <xaviercarty@gmail.com>
2020-08-20 09:49:50 -0400

    add minMax

---
+++ MinMaxStack.js
@@ -0,0 +1,41 @@
+class MinMaxStack {
+	
+	constructor(){
+		this.stack = [] 
+		this.minMaxStack = [] 
+		
+	}
+  peek() {
+    // Write your code here.
+	 return this.stack[this.stack.length-1]
+	
+  }
+
+  pop() {
+    // Write your code here.
+		this.minMaxStack.pop()
+		return this.stack.pop()
+  }
+
+  push(number) {
+    // Write your code here.
+		const newMinMax = {min: number, max: number}
+		if(this.minMaxStack.length){
+			const lastMinMax = this.minMaxStack[this.minMaxStack.length -1]
+			newMinMax.max = Math.max(lastMinMax.max, number)
+			newMinMax.min = Math.min(lastMinMax.min, number)
+		}
+		this.minMaxStack.push(newMinMax)
+		this.stack.push(number)
+  }
+
+  getMin() {
+    // Write your code here.
+		return this.minMaxStack[this.minMaxStack.length - 1].min
+  }
+
+  getMax() {
+    // Write your code here.
+		return this.minMaxStack[this.minMaxStack.length - 1].max
+  }
+}

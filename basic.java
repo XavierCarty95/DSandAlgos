ebe63dc7eb5ed822f87e88e155064fceaf831e38
XavierCarty95 <xaviercarty@gmail.com>
2020-08-30 15:00:28 -0400

    save this

---
+++ basic.java
@@ -0,0 +1,26 @@
+
+public class MasterJavaJ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int Jenny, Danny, Claire;
+		   Jenny = 14; 
+		   Danny = 6; 
+		   Claire = 6; 
+		   
+		   
+		   if(Jenny > Danny) {
+			   System.out.println("Jenny is older than Danny");
+		   };
+		   
+		   if(Jenny < Danny) {
+			   System.out.println("Danny is older than Jenny");
+		   };
+		   if(Danny == Jenny) {
+			   System.out.println("Danny is the same age");
+		   };
+		   if(Danny == Claire) {
+			   System.out.println("Danny is the same age as Claire");
+		   };
+	}
+}

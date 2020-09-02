3cb0da39eadad3562f4464485721e11749e43d7e
XavierCarty95 <xaviercarty@gmail.com>
2020-09-02 10:38:47 -0400

    committing

---
+++ ifelse.java
@@ -0,0 +1,24 @@
+
+		  int temperature = 85; 
+		  boolean sunny = false; 
+		  
+		  
+		  if(temperature > 90) {
+			  System.out.println("It hot");
+			  if(sunny == true) {
+				  System.out.println("it is also sunny outside ");
+			  } else {
+				  System.out.println("it cloudly outside");
+			  }
+		  }
+		  
+		  else {
+
+			  System.out.println("It is not hot outside");
+			  if(sunny == true) {
+				  System.out.println("it brick chill");
+			  } else {
+				  System.out.println("it is pretty brick");
+				 
+			  }
+		  }

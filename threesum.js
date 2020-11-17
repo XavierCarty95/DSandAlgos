553a97cfa2986a1f850bddf5c1891af60ef31568
XavierCarty95 <xaviercarty@gmail.com>
2020-11-17 11:46:30 -0500

    three sum

--- threesum.js
+++ threesum.js
@@ -1,29 +1,37 @@
-const threeSum = (A) => {
-    A.sort((a, b) => a - b);
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+  let output = []
+  nums.sort((a,b) => a - b);
   
-    const allThreeSums = new Set();
-    const secondToLastIndex = A.length - 2;
-  
-    for (let i = 0; i < secondToLastIndex; i++) {
-      findTwoSum(i, A, allThreeSums);
-    }
-  
-    return Array.from(allThreeSums);
-  }
-  
-  const findTwoSum = (rootIndex, A, allThreeSums) => {
-    let left = rootIndex + 1;
-    let right = A.length - 1;
-  
-    while (left < right) {
-      const threeNumberSum = A[rootIndex] + A[left] + A[right];
-  
-      if (threeNumberSum == 0) {
-        allThreeSums.add([A[rootIndex], A[left++], A[right--]]);
-      } else if (threeNumberSum < 0) {
-        left++;
-      } else {
-        right--;
+  for(let i = 0; i < nums.length - 2; i++){
+      let left = i + 1; 
+      let right = nums.length - 1;
+      if(i > 0 && nums[i] == nums[i-1]) continue;
+      
+      while(left < right){
+         let total = nums[i] + nums[left] + nums[right];    
+         if(total === 0){
+             output.push([nums[i], nums[left], nums[right]]);
+             while(left < right && nums[right-1] == nums[right]){
+                 right--;
+             }
+             while(left < right && nums[left+1] == nums[left]){
+                 left++;
+             }
+             right--;
+             left++;
+          } else if(total > 0){
+              
+              right--;
+              
+          } else {
+              left++;
       }
-    }
-  }
+   }
+  }   
+  return output;
+};
